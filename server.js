e060cd5a33d5d9a6d700011478f1ddec2963a40d
Sudiksa17 <sudiksha1701@gmail.com>
2019-03-11 00:28:23 +0530

    projects added.

--- server.js
+++ server.js
@@ -29,6 +29,13 @@ app.get('/about',(req,res)=>{
   })
 })
 
+app.get('/projects',(req,res)=>{
+  res.render('project.hbs',{
+    pageTitle: 'Projects',
+    welcomeMsg: 'Maintain your portfolio here.'
+  })
+})
+
 app.get('/bad',(req,res)=>{
   res.send({
     errorMessage: 'error'
